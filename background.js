4de27c62f29d388c618da86800e9880dc60e85ca
Anthony Hoang <hoanganthony2207@gmail.com>
2024-08-20 11:52:21 -0400

    completed

---
+++ background.js
@@ -0,0 +1,34 @@
+const urlsToBlock = [
+    "*://*.doubleclick.net/*",
+    "*://*.popads.net/*",
+    "*://*.adf.ly/*",
+    "*://*.adsterra.com/*",
+    "*://*.propellerads.com/*",
+    "*://*.onclickads.net/*",
+    "*://*.mgid.com/*",
+    "*://*.taboola.com/*",
+    "*://*.revcontent.com/*",
+    "*://*.outbrain.com/*",
+    "*://*.zedo.com/*",
+    "*://*.yavli.com/*",
+    "*://*.juicyads.com/*",
+    "*://*.adnetworkperformance.com/*",
+    "*://*.adsrvmedia.net/*",
+    "*://*.content.ad/*",
+    "*://*.exoclick.com/*",
+    "*://*.infolinks.com/*",
+    "*://*.trafficfactory.biz/*",
+    "*://*.adk2x.com/*"
+];
+
+chrome.declarativeNetRequest.updateDynamicRules({
+    addRules: urlsToBlock.map((url, index) => ({
+        id: index + 1,
+        priority: 1,
+        action: { type: 'block' },
+        condition: {
+            urlFilter: url
+        }
+    })),
+    removeRuleIds: Array.from({ length: urlsToBlock.length }, (_, index) => index + 1)
+});
